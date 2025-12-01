cca4f3dd96a74ec7213d70fe5f44b3c6fd8edfa0
Vasilis Balafas <me@vasilis.pw>
2025-12-01 11:30:42 +0200

    Update benchmarks and output directory in `runs_exps_parallel.py`
    - Commented out the `sudoku` and `sudoku_gt` benchmarks to exclude them from testing.
    - Changed the output directory name from `solution_variance_output` to `solution_variance_output22222` for differentiation in result storage.

--- runs_exps_parallel.py
+++ runs_exps_parallel.py
@@ -762,8 +762,8 @@ def main(num_runs=10):
     
     # Define benchmarks to test
     benchmarks = [
-         'sudoku_gt',
-        'sudoku',
+        #  'sudoku_gt',
+        # 'sudoku',
         'graph_coloring_register',
         'examtt_v1',
         'examtt_v2',
@@ -798,7 +798,7 @@ def main(num_runs=10):
     aggregated_metrics = {}  # Key: (benchmark, num_solutions, approach), Value: list of metrics
     
     # Setup output directory and intermediate results files
-    output_dir = 'solution_variance_output'
+    output_dir = 'solution_variance_output22222'
     os.makedirs(output_dir, exist_ok=True)
     
     intermediate_csv_path = f"{output_dir}/intermediate_results.csv"
